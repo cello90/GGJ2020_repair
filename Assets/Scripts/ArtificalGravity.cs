94c89e79c5fc9ed8cce58cde2b1221a6a5fbbdef
Kermit <Hunting4Code@protonmail.com>
2020-01-31 19:06:54 -0800

    yyy

---
+++ Assets/Scripts/ArtificalGravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtificalGravity : MonoBehaviour
+{
+
+    private Transform _gc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gc = GameObject.FindGameObjectWithTag('test');
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

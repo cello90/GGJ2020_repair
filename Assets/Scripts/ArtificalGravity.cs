e191243740f629cc235a64f039dab10509b23e6a
Kermit <Hunting4Code@protonmail.com>
2020-01-31 19:10:24 -0800

    iut67r

--- Assets/Scripts/ArtificalGravity.cs
+++ Assets/Scripts/ArtificalGravity.cs
@@ -6,11 +6,13 @@ public class ArtificalGravity : MonoBehaviour
 {
 
     private Transform _gc;
+    private Rigidbody _rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        _gc = GameObject.FindGameObjectWithTag('test');
+        _gc = GameObject.FindGameObjectWithTag("Gravity Center").transform;
+        _rb = GetComponent<GameObject>
     }
 
     // Update is called once per frame

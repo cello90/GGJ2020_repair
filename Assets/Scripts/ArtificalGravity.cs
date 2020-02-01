6746b39427e80f6c9da7cd0d49fabe730aaa707d
Kermit <Hunting4Code@protonmail.com>
2020-01-31 19:27:44 -0800

    Gravity Handler

--- Assets/Scripts/ArtificalGravity.cs
+++ Assets/Scripts/ArtificalGravity.cs
@@ -2,29 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class ArtificalGravity : MonoBehaviour
 {
 
     public float gravityMult = 1;
     private float g = -9.81f;
     private Transform _gc;
-    private Rigidbody _rb;
+    private Rigidbody2D _rb;
 
     // Start is called before the first frame update
     void Start()
     {
         _gc = GameObject.FindGameObjectWithTag("Gravity Center").transform;
-        _rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody2D>();
+        g = Physics.gravity.y;
     }
 
     // FixedUpdate is called once per Physics loop
     void FixedUpdate()
     {
-        
+        UpdateGravity();
     }
 
-    void updateGravity()
+    void UpdateGravity()
     {
-        _rb.AddForce
+        _rb.AddForce((_gc.position - transform.position).normalized * g * GravityMult());
+    }
+
+    float GravityMult()
+    {
+        return Vector3.Distance(transform.position, _gc.position);
     }
 }
